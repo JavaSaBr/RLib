acd089fc3ba71ee1295399ac1c1670d25fc23158
Ronn <javasabr@gmail.com>
2013-07-06 13:30:31 +0300

    +пару утильных методов по строкам

--- RLib/src/rlib/geom/Rotation.java
+++ RLib/src/rlib/geom/Rotation.java
@@ -13,27 +13,32 @@ public class Rotation
 	{
 		return new Rotation();
 	}
-	
+
 	public static Rotation newInstance(float x, float y, float z, float w)
 	{
 		return new Rotation(x, y, z, w);
 	}
-	
+
 	public static Rotation newInstance(float[] vals)
 	{
 		return new Rotation(vals[0], vals[1], vals[2], vals[3]);
 	}
-	
+
+	public static Rotation newInstance(float xAngle, float yAngle, float zAngle)
+	{
+		return new Rotation().fromAngles(xAngle, yAngle, zAngle);
+	}
+
 	private float x;
 	private float y;
 	private float z;
 	private float w;
-	
+
 	private Rotation()
 	{
 		w = 1;
 	}
-	
+
 	private Rotation(float x, float y, float z, float w)
 	{
 		this.x = x;
@@ -41,39 +46,36 @@ public class Rotation
 		this.z = z;
 		this.w = w;
 	}
-	
+
 	/**
 	 * <code>dot</code> calculates and returns the dot product of this
 	 * quaternion with that of the parameter quaternion.
-	 *
-	 * @param q
-	 *            the quaternion to calculate the dot product of.
+	 * 
+	 * @param q the quaternion to calculate the dot product of.
 	 * @return the dot product of this and the parameter quaternion.
 	 */
 	public float dot(Rotation q)
 	{
 		return w * q.w + x * q.x + y * q.y + z * q.z;
 	}
-	
+
 	/**
 	 * Получение вектора нужного типа.
 	 * 
-	 * @param type
-	 *            тип вектора.
-	 * @param store
-	 *            контейнер.
+	 * @param type тип вектора.
+	 * @param store контейнер.
 	 * @return вычисленный вектор.
 	 */
 	public Vector getRotationColumn(VectorType type, Vector store)
 	{
 		if(store == null)
 			store = Vector.newInstance();
-		
+
 		float norm = norm();
-		
+
 		if(norm != 1.0f)
 			norm = ExtMath.invSqrt(norm);
-		
+
 		float xx = x * x * norm;
 		float xy = x * y * norm;
 		float xz = x * z * norm;
@@ -83,7 +85,7 @@ public class Rotation
 		float yw = y * w * norm;
 		float zz = z * z * norm;
 		float zw = z * w * norm;
-		
+
 		switch(type)
 		{
 			case LEFT:
@@ -91,7 +93,7 @@ public class Rotation
 				store.setX(1 - 2 * (yy + zz));
 				store.setY(2 * (xy + zw));
 				store.setZ(2 * (xz - yw));
-				
+
 				break;
 			}
 			case UP:
@@ -99,7 +101,7 @@ public class Rotation
 				store.setX(2 * (xy - zw));
 				store.setY(1 - 2 * (xx + zz));
 				store.setZ(2 * (yz + xw));
-				
+
 				break;
 			}
 			case DIRECTION:
@@ -109,10 +111,10 @@ public class Rotation
 				store.setZ(1 - 2 * (xx + yy));
 			}
 		}
-		
+
 		return store;
 	}
-	
+
 	/**
 	 * @return степень разворота по оси w.
 	 */
@@ -120,7 +122,7 @@ public class Rotation
 	{
 		return w;
 	}
-	
+
 	/**
 	 * @return степень разворота по оси х.
 	 */
@@ -128,7 +130,7 @@ public class Rotation
 	{
 		return x;
 	}
-	
+
 	/**
 	 * @return степень разворота по оси y.
 	 */
@@ -136,7 +138,7 @@ public class Rotation
 	{
 		return y;
 	}
-	
+
 	/**
 	 * @return степень разворота по оси z.
 	 */
@@ -144,9 +146,10 @@ public class Rotation
 	{
 		return z;
 	}
-	
+
 	/**
-	 * <code>norm</code> returns the norm of this quaternion. This is the dot product of this quaternion with itself.
+	 * <code>norm</code> returns the norm of this quaternion. This is the dot
+	 * product of this quaternion with itself.
 	 * 
 	 * @return the norm of the quaternion.
 	 */
@@ -154,35 +157,33 @@ public class Rotation
 	{
 		return w * w + x * x + y * y + z * z;
 	}
-	
+
 	public Rotation set(Rotation rotation)
 	{
 		this.x = rotation.x;
 		this.y = rotation.y;
 		this.z = rotation.z;
 		this.w = rotation.w;
-		
+
 		return this;
 	}
-	
+
 	/**
-	 * @param степень
-	 *            разворота по оси w.
+	 * @param степень разворота по оси w.
 	 */
 	public final void setW(float w)
 	{
 		this.w = w;
 	}
-	
+
 	/**
-	 * @param степень
-	 *            разворота по оси х.
+	 * @param степень разворота по оси х.
 	 */
 	public final void setX(float x)
 	{
 		this.x = x;
 	}
-	
+
 	public void setXYZW(float x, float y, float z, float w)
 	{
 		this.x = x;
@@ -190,33 +191,29 @@ public class Rotation
 		this.z = z;
 		this.w = w;
 	}
-	
+
 	/**
-	 * @param степень
-	 *            разворота по оси y.
+	 * @param степень разворота по оси y.
 	 */
 	public final void setY(float y)
 	{
 		this.y = y;
 	}
-	
+
 	/**
-	 * @param степень
-	 *            разворота по оси z.
+	 * @param степень разворота по оси z.
 	 */
 	public final void setZ(float z)
 	{
 		this.z = z;
 	}
-	
+
 	/**
 	 * Sets the values of this quaternion to the slerp from itself to q2 by
 	 * changeAmnt
-	 *
-	 * @param q2
-	 *            Final interpolation value
-	 * @param changeAmnt
-	 *            The amount diffrence
+	 * 
+	 * @param q2 Final interpolation value
+	 * @param changeAmnt The amount diffrence
 	 */
 	public void slerp(Rotation q2, float changeAmnt)
 	{
@@ -224,9 +221,9 @@ public class Rotation
 		{
 			return;
 		}
-		
+
 		float result = (this.x * q2.x) + (this.y * q2.y) + (this.z * q2.z) + (this.w * q2.w);
-		
+
 		if(result < 0.0f)
 		{
 			// Negate the second quaternion and the result of the dot product
@@ -236,11 +233,11 @@ public class Rotation
 			q2.w = -q2.w;
 			result = -result;
 		}
-		
+
 		// Set the first and second scale for the interpolation
 		float scale0 = 1 - changeAmnt;
 		float scale1 = changeAmnt;
-		
+
 		// Check if the angle between the 2 quaternions was big enough to
 		// warrant such calculations
 		if((1 - result) > 0.1f)
@@ -249,13 +246,13 @@ public class Rotation
 			// of that angle
 			float theta = ExtMath.acos(result);
 			float invSinTheta = 1f / ExtMath.sin(theta);
-			
+
 			// Calculate the scale for q1 and q2, according to the angle and
 			// it's sine value
 			scale0 = ExtMath.sin((1 - changeAmnt) * theta) * invSinTheta;
 			scale1 = ExtMath.sin((changeAmnt * theta)) * invSinTheta;
 		}
-		
+
 		// Calculate the x, y, z and w values for the quaternion by using a
 		// special
 		// form of linear interpolation for quaternions.
@@ -264,17 +261,14 @@ public class Rotation
 		this.z = (scale0 * this.z) + (scale1 * q2.z);
 		this.w = (scale0 * this.w) + (scale1 * q2.w);
 	}
-	
+
 	/**
 	 * <code>slerp</code> sets this quaternion's value as an interpolation
 	 * between two other quaternions.
-	 *
-	 * @param q1
-	 *            the first quaternion.
-	 * @param q2
-	 *            the second quaternion.
-	 * @param t
-	 *            the amount to interpolate between the two quaternions.
+	 * 
+	 * @param q1 the first quaternion.
+	 * @param q2 the second quaternion.
+	 * @param t the amount to interpolate between the two quaternions.
 	 */
 	public Rotation slerp(Rotation q1, Rotation q2, float t)
 	{
@@ -284,9 +278,9 @@ public class Rotation
 			this.set(q1);
 			return this;
 		}
-		
+
 		float result = (q1.x * q2.x) + (q1.y * q2.y) + (q1.z * q2.z) + (q1.w * q2.w);
-		
+
 		if(result < 0.0f)
 		{
 			// Negate the second quaternion and the result of the dot product
@@ -296,11 +290,11 @@ public class Rotation
 			q2.w = -q2.w;
 			result = -result;
 		}
-		
+
 		// Set the first and second scale for the interpolation
 		float scale0 = 1 - t;
 		float scale1 = t;
-		
+
 		// Check if the angle between the 2 quaternions was big enough to
 		// warrant such calculations
 		if((1 - result) > 0.1f)
@@ -308,13 +302,13 @@ public class Rotation
 			// and then store the sin() of that angle
 			float theta = ExtMath.acos(result);
 			float invSinTheta = 1f / ExtMath.sin(theta);
-			
+
 			// Calculate the scale for q1 and q2, according to the angle and
 			// it's sine value
 			scale0 = ExtMath.sin((1 - t) * theta) * invSinTheta;
 			scale1 = ExtMath.sin((t * theta)) * invSinTheta;
 		}
-		
+
 		// Calculate the x, y, z and w values for the quaternion by using a
 		// special
 		// form of linear interpolation for quaternions.
@@ -322,11 +316,11 @@ public class Rotation
 		this.y = (scale0 * q1.y) + (scale1 * q2.y);
 		this.z = (scale0 * q1.z) + (scale1 * q2.z);
 		this.w = (scale0 * q1.w) + (scale1 * q2.w);
-		
+
 		// Return the interpolated quaternion
 		return this;
 	}
-	
+
 	/**
 	 * Высчитать интерполяцию между 2мя разворотами.
 	 * 
@@ -344,24 +338,24 @@ public class Rotation
 			set(start);
 			return this;
 		}
-		
+
 		// рассчитываем косинус угла
 		float result = start.dot(end);
-		
+
 		// если он отрицательный
 		if(result < 0.0f)
 		{
 			// переворачиваем разворот
 			end.negateLocal();
-			
+
 			// переворачиваем результат
 			result = -result;
 		}
-		
+
 		// определяем маштаб
 		float startScale = 1 - done;
 		float endScale = done;
-		
+
 		// если есть смысл использовать сфеерическую интерполяцию
 		if(!forceLinear && (1 - result) > 0.1f)
 		{
@@ -372,21 +366,23 @@ public class Rotation
 			startScale = ExtMath.sin((1 - done) * theta) * invSinTheta;
 			endScale = ExtMath.sin((done * theta)) * invSinTheta;
 		}
-		
+
 		// применяем интерполяцию
 		this.x = (startScale * start.getX()) + (endScale * end.getX());
 		this.y = (startScale * start.getY()) + (endScale * end.getY());
 		this.z = (startScale * start.getZ()) + (endScale * end.getZ());
 		this.w = (startScale * start.getW()) + (endScale * end.getW());
-		
+
 		return this;
 	}
-	
+
 	/**
-	 * <code>toAngleAxis</code> sets a given angle and axis to that represented by the current quaternion. The values are stored as follows: The axis is provided as a parameter and built by the method, the angle is returned as a float.
+	 * <code>toAngleAxis</code> sets a given angle and axis to that represented
+	 * by the current quaternion. The values are stored as follows: The axis is
+	 * provided as a parameter and built by the method, the angle is returned as
+	 * a float.
 	 * 
-	 * @param axisStore
-	 *            the object we'll store the computed axis in.
+	 * @param axisStore the object we'll store the computed axis in.
 	 * @return the angle of rotation in radians.
 	 */
 	public final float toAngleAxis(Vector axisStore)
@@ -417,7 +413,7 @@ public class Rotation
 
 		return angle;
 	}
-	
+
 	/**
 	 * Конвектирование квантерниона в матрицу 3х3
 	 * 
@@ -425,24 +421,24 @@ public class Rotation
 	 * @return результат в виде матрицы.
 	 */
 	public final Matrix3f toRotationMatrix(Matrix3f result)
-	{		
+	{
 		// получаем нормаль этого квантерниона
 		float norm = norm();
-		
+
 		// we explicitly test norm against one here, saving a division
 		// at the cost of a test and branch. Is it worth it?
 		float s = (norm == 1f) ? 2f : (norm > 0f) ? 2f / norm : 0;
-		
+
 		float x = getX();
 		float y = getY();
 		float z = getZ();
 		float w = getW();
-		
+
 		// производим промежуточные рассчеты
 		float xs = x * s;
 		float ys = y * s;
 		float zs = z * s;
-		float xx = x * xs; 
+		float xx = x * xs;
 		float xy = x * ys;
 		float xz = x * zs;
 		float xw = w * xs;
@@ -451,17 +447,17 @@ public class Rotation
 		float yw = w * ys;
 		float zz = z * zs;
 		float zw = w * zs;
-		
+
 		// рассчитываем итоговые значения матрицы
 		result.set(1 - (yy + zz), xy - zw, xz + yw, xy + zw, 1 - (xx + zz), yz - xw, xz - yw, yz + xw, 1 - (xx + yy));
-		
+
 		// возвращаем результат
 		return result;
 	}
-	
+
 	/**
 	 * Приминение разворота на вектор.
-	 *
+	 * 
 	 * @param vector вектор, который надо развернуть.
 	 * @return полученный вектор.
 	 */
@@ -470,94 +466,97 @@ public class Rotation
 		float vectorX = vector.getX();
 		float vectorY = vector.getY();
 		float vectorZ = vector.getZ();
-		
+
 		float x = getX();
 		float y = getY();
 		float z = getZ();
 		float w = getW();
-		
-		vector.setX(w * w * vectorX + 2 * y * w * vectorZ - 2 * z * w * vectorY + x * x * vectorX + 2 * y * x * vectorY + 2 * z * x * vectorZ - z * z * vectorX - y * y * vectorX);
-		vector.setY(2 * x * y * vectorX + y * y * vectorY + 2 * z * y * vectorZ + 2 * w * z * vectorX - z * z * vectorY + w * w * vectorY - 2 * x * w * vectorZ - x * x * vectorY);
-		vector.setZ(2 * x * z * vectorX + 2 * y * z * vectorY + z * z * vectorZ - 2 * w * y * vectorX - y * y * vectorZ + 2 * w * x * vectorY - x * x * vectorZ + w * w * vectorZ);
-		
+
+		vector.setX(w * w * vectorX + 2 * y * w * vectorZ - 2 * z * w * vectorY + x * x * vectorX + 2 * y * x * vectorY + 2 * z * x
+				* vectorZ - z * z * vectorX - y * y * vectorX);
+		vector.setY(2 * x * y * vectorX + y * y * vectorY + 2 * z * y * vectorZ + 2 * w * z * vectorX - z * z * vectorY + w * w * vectorY
+				- 2 * x * w * vectorZ - x * x * vectorY);
+		vector.setZ(2 * x * z * vectorX + 2 * y * z * vectorY + z * z * vectorZ - 2 * w * y * vectorX - y * y * vectorZ + 2 * w * x
+				* vectorY - x * x * vectorZ + w * w * vectorZ);
+
 		return vector;
 	}
-	
-	 /**
-     * Расчет разворота по углам в 3х осях.
-     * 
-     * @param angles угол наклона по осям.
-     */
-    public final Rotation fromAngles(float[] angles) 
-    {
-    	return fromAngles(angles[0],  angles[1],  angles[2]);
-    }
-	
-	 /**
-     * Расчет разворота по углам в 3х осях.
-     * 
-     * @param angleX угол по оси X.
-     * @param yAngle угол по оси Y.
-     * @param zAngle угол по оси Z.
-     */
-    public final Rotation fromAngles(float angleX, float yAngle, float zAngle) 
-    {
-        float angle = zAngle * 0.5f;
-        
-        float sinZ = ExtMath.sin(angle);
-        float cosZ = ExtMath.cos(angle);
-        
-        angle = yAngle * 0.5f;
-        
-        float sinY = ExtMath.sin(angle);
-        float cosY = ExtMath.cos(angle);
-        
-        angle = angleX * 0.5f;
-        
-        float sinX = ExtMath.sin(angle);
-        float cosX = ExtMath.cos(angle);
-        
-        float cosYXcosZ = cosY * cosZ;
-        float sinYXsinZ = sinY * sinZ;
-        float cosYXsinZ = cosY * sinZ;
-        float sinYXcosZ = sinY * cosZ;
 
-        w = (cosYXcosZ * cosX - sinYXsinZ * sinX);
-        x = (cosYXcosZ * sinX + sinYXsinZ * cosX);
-        y = (sinYXcosZ * cosX + cosYXsinZ * sinX);
-        z = (cosYXsinZ * cosX - sinYXcosZ * sinX);
+	/**
+	 * Расчет разворота по углам в 3х осях.
+	 * 
+	 * @param angles угол наклона по осям.
+	 */
+	public final Rotation fromAngles(float[] angles)
+	{
+		return fromAngles(angles[0], angles[1], angles[2]);
+	}
 
-        normalizeLocal();
-        
-        return this;
-    }
-    
-    /**
-     * Нормализация текущего разворота.
-     */
-    public final Rotation normalizeLocal() 
-    {
-        float norm = ExtMath.invSqrt(norm());
-        
-        x *= norm;
-        y *= norm;
-        z *= norm;
-        w *= norm;
-        
-        return this;
-    }
+	/**
+	 * Расчет разворота по углам в 3х осях.
+	 * 
+	 * @param angleX угол по оси X.
+	 * @param yAngle угол по оси Y.
+	 * @param zAngle угол по оси Z.
+	 */
+	public final Rotation fromAngles(float angleX, float yAngle, float zAngle)
+	{
+		float angle = zAngle * 0.5f;
+
+		float sinZ = ExtMath.sin(angle);
+		float cosZ = ExtMath.cos(angle);
+
+		angle = yAngle * 0.5f;
+
+		float sinY = ExtMath.sin(angle);
+		float cosY = ExtMath.cos(angle);
+
+		angle = angleX * 0.5f;
+
+		float sinX = ExtMath.sin(angle);
+		float cosX = ExtMath.cos(angle);
+
+		float cosYXcosZ = cosY * cosZ;
+		float sinYXsinZ = sinY * sinZ;
+		float cosYXsinZ = cosY * sinZ;
+		float sinYXcosZ = sinY * cosZ;
+
+		w = (cosYXcosZ * cosX - sinYXsinZ * sinX);
+		x = (cosYXcosZ * sinX + sinYXsinZ * cosX);
+		y = (sinYXcosZ * cosX + cosYXsinZ * sinX);
+		z = (cosYXsinZ * cosX - sinYXcosZ * sinX);
+
+		normalizeLocal();
+
+		return this;
+	}
+
+	/**
+	 * Нормализация текущего разворота.
+	 */
+	public final Rotation normalizeLocal()
+	{
+		float norm = ExtMath.invSqrt(norm());
+
+		x *= norm;
+		y *= norm;
+		z *= norm;
+		w *= norm;
+
+		return this;
+	}
 
 	@Override
 	public final int hashCode()
 	{
 		final int prime = 31;
 		int result = 1;
-		
+
 		result = prime * result + Float.floatToIntBits(w);
 		result = prime * result + Float.floatToIntBits(x);
 		result = prime * result + Float.floatToIntBits(y);
 		result = prime * result + Float.floatToIntBits(z);
-		
+
 		return result;
 	}
 
@@ -566,15 +565,15 @@ public class Rotation
 	{
 		if(this == obj)
 			return true;
-		
+
 		if(obj == null)
 			return false;
-		
+
 		if(getClass() != obj.getClass())
 			return false;
-		
+
 		Rotation other = (Rotation) obj;
-		
+
 		if(Float.floatToIntBits(w) != Float.floatToIntBits(other.w))
 			return false;
 		if(Float.floatToIntBits(x) != Float.floatToIntBits(other.x))
@@ -583,10 +582,10 @@ public class Rotation
 			return false;
 		if(Float.floatToIntBits(z) != Float.floatToIntBits(other.z))
 			return false;
-		
+
 		return true;
 	}
-	
+
 	/**
 	 * @return перевернуть значения.
 	 */
@@ -596,7 +595,7 @@ public class Rotation
 		y = -y;
 		z = -z;
 		w = -w;
-		
+
 		return this;
 	}
 
--- RLib/src/rlib/util/Strings.java
+++ RLib/src/rlib/util/Strings.java
@@ -8,10 +8,9 @@ import java.util.regex.Pattern;
 
 import rlib.logging.Loggers;
 
-
 /**
  * Набор методов для работы со строками
- *
+ * 
  * @author Ronn
  * @created 27.03.2012
  */
@@ -19,30 +18,31 @@ public class Strings
 {
 	/** экземпляр пустой строки */
 	public static final String EMPTY = "".intern();
-	
+
 	/** экземпляр пустого массива строк */
 	public static final String[] EMPTY_ARRAY = new String[0];
 
 	/** создаем регулярку для проверки почты */
-	public static final Pattern emailPattern = Pattern.compile("^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$", 
-			Pattern.DOTALL | Pattern.CASE_INSENSITIVE | Pattern.MULTILINE);
-	
+	public static final Pattern emailPattern = Pattern.compile(
+			"^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$", Pattern.DOTALL
+					| Pattern.CASE_INSENSITIVE | Pattern.MULTILINE);
+
 	/** класс для получения хэша */
 	private static final MessageDigest hashMD5 = getHashMD5();
-	
+
 	/**
 	 * Проверка на корректность имейла.
-	 *
+	 * 
 	 * @param email имеил.
 	 * @return корректно ли введен.
 	 */
 	public static boolean checkEmail(String email)
 	{
 		Matcher matcher = emailPattern.matcher(email);
-		
+
 		return matcher.matches();
 	}
-	
+
 	/**
 	 * @return получаение алгоритма хеша.
 	 */
@@ -56,27 +56,57 @@ public class Strings
 		{
 			Loggers.warning("Strings", e);
 		}
-		
+
 		return null;
 	}
 
 	/**
 	 * Рассчет длинны строки для пакета
-	 *
+	 * 
 	 * @param string
 	 * @return length
 	 */
-	public static int length(String string)
+	public static int byteCount(String string)
 	{
 		if(string == null || string.isEmpty())
 			return 2;
 
 		return string.length() * 2 + 2;
 	}
-	
+
+	/**
+	 * @return является ли строка пустой.
+	 */
+	public static boolean isEmpty(String string)
+	{
+		return string == null || string.isEmpty();
+	}
+
+	/**
+	 * Сравнение 2х строк.
+	 */
+	public static boolean equals(String first, String second)
+	{
+		if(first == null || second == null)
+			return false;
+
+		return first.equals(second);
+	}
+
+	/**
+	 * Сравнение 2х строк без учета регистра.
+	 */
+	public static boolean equalsIgnoreCase(String first, String second)
+	{
+		if(first == null || second == null)
+			return false;
+
+		return first.equalsIgnoreCase(second);
+	}
+
 	/**
 	 * Получение хэша пароля.
-	 *
+	 * 
 	 * @param password пароль.
 	 * @return хэш пароля.
 	 */
@@ -88,7 +118,7 @@ public class Strings
 		// конвиктируем в хэш
 		return new BigInteger(1, hashMD5.digest()).toString(16);
 	}
-	
+
 	/**
 	 * Генерирование случайной строки указанной длинны.
 	 * 
@@ -98,10 +128,10 @@ public class Strings
 	public static String generate(int length)
 	{
 		char[] array = new char[length];
-		
+
 		for(int i = 0; i < length; i++)
 			array[i] = (char) Rnd.nextInt('a', 'z');
-		
+
 		return String.valueOf(array);
 	}
 }
