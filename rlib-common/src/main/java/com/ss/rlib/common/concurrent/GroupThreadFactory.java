37a75fa4ac768fb399c57874c5501857a55583ee
JavaSaBr <javasabr@gmail.com>
2018-11-23 09:36:42 +0300

    updated some API

--- rlib-common/src/main/java/com/ss/rlib/common/concurrent/GroupThreadFactory.java
+++ rlib-common/src/main/java/com/ss/rlib/common/concurrent/GroupThreadFactory.java
@@ -2,20 +2,23 @@ package com.ss.rlib.common.concurrent;
 
 import com.ss.rlib.common.concurrent.atomic.AtomicInteger;
 import com.ss.rlib.common.util.ClassUtils;
-import com.ss.rlib.common.concurrent.atomic.AtomicInteger;
-import com.ss.rlib.common.util.ClassUtils;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Constructor;
 import java.util.concurrent.ThreadFactory;
 
 /**
- * The implementation of the {@link ThreadFactory} for creating some threads in the same thread
+ * The implementation of the {@link ThreadFactory} to create threads in the same thread
  * group.
  *
  * @author JavaSaBr
  */
 public class GroupThreadFactory implements ThreadFactory {
+
+    public interface ThreadConstructor {
+
+        @NotNull Thread create(@NotNull ThreadGroup group, @NotNull Runnable runnable, @NotNull String name);
+    }
 
     /**
      * The order of the next thread.
@@ -33,33 +36,56 @@ public class GroupThreadFactory implements ThreadFactory {
     private final ThreadGroup group;
 
     /**
-     * The constructor for making threads.
+     * The thread's constructor.
      */
-    private final Constructor<? extends Thread> constructor;
+    private final ThreadConstructor constructor;
 
     /**
      * The priority of these threads.
      */
     private final int priority;
 
-    /**
-     * Instantiates a new Group thread factory.
-     *
-     * @param name     the name
-     * @param cs       the cs
-     * @param priority the priority
-     */
-    public GroupThreadFactory(@NotNull final String name, @NotNull final Class<? extends Thread> cs, final int priority) {
-        this.constructor = ClassUtils.getConstructor(cs, ThreadGroup.class, Runnable.class, String.class);
+    public GroupThreadFactory(@NotNull String name) {
+        this(name, Thread::new, Thread.NORM_PRIORITY);
+    }
+
+    public GroupThreadFactory(@NotNull String name, int priority) {
+        this(name, Thread::new, priority);
+    }
+
+    public GroupThreadFactory(@NotNull String name, @NotNull ThreadConstructor constructor, int priority) {
+        this.constructor = constructor;
         this.priority = priority;
         this.name = name;
         this.group = new ThreadGroup(name);
         this.ordinal = new AtomicInteger();
     }
 
+    @Deprecated
+    public GroupThreadFactory(@NotNull String name, @NotNull Class<? extends Thread> cs, int priority) {
+        this.priority = priority;
+        this.name = name;
+        this.group = new ThreadGroup(name);
+        this.ordinal = new AtomicInteger();
+        this.constructor = new ThreadConstructor() {
+
+            Constructor<? extends Thread> constructor =
+                ClassUtils.getConstructor(cs, ThreadGroup.class, Runnable.class, String.class);
+
+            @Override
+            public @NotNull Thread create(
+                @NotNull ThreadGroup group,
+                @NotNull Runnable runnable,
+                @NotNull String name
+            ) {
+                return ClassUtils.newInstance(constructor, group, runnable, name);
+            }
+        };
+    }
+
     @Override
-    public Thread newThread(@NotNull final Runnable runnable) {
-        final Thread thread = ClassUtils.newInstance(constructor, group, runnable, name + "-" + ordinal.incrementAndGet());
+    public @NotNull Thread newThread(@NotNull Runnable runnable) {
+        var thread = constructor.create(group, runnable, name + "-" + ordinal.incrementAndGet());
         thread.setPriority(priority);
         return thread;
     }
