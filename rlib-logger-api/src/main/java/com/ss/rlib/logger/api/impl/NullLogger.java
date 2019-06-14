1219fb4262d784411a5d5fba81374cd24238eb06
Alex Brui <abrui@netgear.com>
2019-06-14 16:07:36 +0300

    refactoring

--- rlib-logger-api/src/main/java/com/ss/rlib/logger/api/impl/NullLogger.java
+++ rlib-logger-api/src/main/java/com/ss/rlib/logger/api/impl/NullLogger.java
@@ -8,76 +8,25 @@ import org.jetbrains.annotations.Nullable;
 public final class NullLogger implements Logger {
 
     @Override
-    public void print(@NotNull LoggerLevel level, @NotNull Object owner, @NotNull String message) {
+    public void print(
+        @NotNull LoggerLevel level, @NotNull Object owner, @NotNull String message
+    ) {
     }
 
     @Override
     public void print(@NotNull LoggerLevel level, @NotNull String message) {
+
     }
 
     @Override
     public void print(
-        @NotNull LoggerLevel level,
-        @NotNull Object owner,
-        @NotNull Throwable exception
+        @NotNull LoggerLevel level, @NotNull Object owner, @NotNull Throwable exception
     ) {
+
     }
 
     @Override
     public void print(@NotNull LoggerLevel level, @NotNull Throwable exception) {
-    }
 
-    @Override
-    public <T> void print(
-        @NotNull LoggerLevel level,
-        @NotNull Object owner,
-        @Nullable T arg,
-        @NotNull Logger.SinFactory<T> messageFactory
-    ) {
-    }
-
-    @Override
-    public <F, S> void print(
-        @NotNull LoggerLevel level,
-        @NotNull Object owner,
-        @Nullable F first,
-        @Nullable S second,
-        @NotNull Logger.BiFactory<F, S> messageFactory
-    ) {
-
-    }
-
-    @Override
-    public <F, S, T> void print(
-        @NotNull LoggerLevel level,
-        @NotNull Object owner,
-        @Nullable F first,
-        @Nullable S second,
-        @Nullable T third,
-        @NotNull Logger.TriFactory<F, S, T> messageFactory
-    ) {
-    }
-
-    @Override
-    public <T> void print(@NotNull LoggerLevel level, @Nullable T arg, @NotNull Logger.SinFactory<T> messageFactory) {
-    }
-
-    @Override
-    public <F, S> void print(
-        @NotNull LoggerLevel level,
-        @Nullable F first,
-        @Nullable S second,
-        @NotNull Logger.BiFactory<F, S> messageFactory
-    ) {
-    }
-
-    @Override
-    public <F, S, T> void print(
-        @NotNull LoggerLevel level,
-        @Nullable F first,
-        @Nullable S second,
-        @Nullable T third,
-        @NotNull Logger.TriFactory<F, S, T> messageFactory
-    ) {
     }
 }
