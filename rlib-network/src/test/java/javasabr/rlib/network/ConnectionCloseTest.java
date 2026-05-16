426fb5e543a2bd868e18644c9644f6d2280a355c
Maksim Kashapov <56276969+crazyrokr@users.noreply.github.com>
2026-05-16 10:56:53 +0200

    remove redundant AwaitUtils

--- rlib-network/src/test/java/javasabr/rlib/network/ConnectionCloseTest.java
+++ rlib-network/src/test/java/javasabr/rlib/network/ConnectionCloseTest.java
@@ -3,11 +3,12 @@ package javasabr.rlib.network;
 import static javasabr.rlib.network.util.NetworkUtils.createAllTrustedClientSslContext;
 import static javasabr.rlib.network.util.NetworkUtils.createSslContext;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 
 import java.net.InetSocketAddress;
+import java.time.Duration;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
-import javasabr.rlib.common.util.AwaitUtils;
 import javasabr.rlib.network.exception.ConnectionClosedException;
 import javasabr.rlib.network.impl.AbstractConnection;
 import javasabr.rlib.network.impl.DefaultConnection;
@@ -80,14 +81,16 @@ public class ConnectionCloseTest extends BaseNetworkTest {
 
       // when
       clientConnection.channel().close();
-      assertThat(AwaitUtils.await(5, TimeUnit.SECONDS, clientConnection::closed))
-          .as("Client connection should be closed prior server side verification")
-          .isTrue();
 
       // then
-      assertThat(AwaitUtils.await(5, TimeUnit.SECONDS, serverConnection::closed))
-          .as("Server connection should be closed after receiving EOF from abruptly closed client channel")
-          .isTrue();
+      assertTimeoutPreemptively(
+          Duration.ofSeconds(5),
+          clientConnection::closed,
+          "Client connection was not properly closed prior server side verification");
+      assertTimeoutPreemptively(
+          Duration.ofSeconds(5),
+          serverConnection::closed,
+          "Server connection was not closed after receiving EOF from abruptly closed client channel");
     }
   }
 }
